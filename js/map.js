81aa3347319f6c300f3f4817db835460b4f3c0d7
Guilherme Guerra <guerrinha@comum.org>
2017-12-11 00:26:07 -0200

    Add map interaction

--- js/map.js
+++ js/map.js
@@ -1,21 +1,30 @@
-var publicSpreadsheetUrl = '1HpZF_eNbrzYRGePK18z4ytoUQX9w-eIfP7hSRwtV8HY';
+$( document ).ready(function() {
+  var loading = true;
+  var publicSpreadsheetUrl = '1HpZF_eNbrzYRGePK18z4ytoUQX9w-eIfP7hSRwtV8HY';
 
-function init() {
-  Tabletop.init({
-    key: publicSpreadsheetUrl,
-    callback: showInfo,
-    simpleSheet: true
-  })
-}
+  function init() {
+    Tabletop.init({
+      key: publicSpreadsheetUrl,
+      callback: showInfo,
+      simpleSheet: true
+    })
+  }
 
-function showInfo(data, tabletop) {
-  console.log("Ready;")
-  $(".state").click(function() {
-    $(this).css({ fill: "#ff0000" });
-    console.log("Dadados: ", data.filter( (e) => e.id == this.id )[0])
-    
-  });
-}
+  function showInfo(data, tabletop) {
+    $(".loading").hide();
 
-window.addEventListener('DOMContentLoaded', init)
+    $(".state").click(function() {
+      $('.state.active').removeClass('active');
 
+      $(this).toggleClass("active");
+      var info = data.filter( (e) => e.id == this.id )[0]
+      $(".title").text(info.state)
+      $(".org").text(info.org)
+      $(".content").text(info.Content)
+      $(".strategy").text(info.strategy)
+
+    });
+  }
+
+  init();
+});
